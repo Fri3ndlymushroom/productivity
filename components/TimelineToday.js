09e072aa6e5fcbb3c172706bb4d4f4d2a15565b1
Moritz Huesser <morihues0@gmail.com>
2021-10-29 07:08:48 +0200

    column layout

--- components/TimelineToday.js
+++ components/TimelineToday.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { ScrollView, View, Text, StyleSheet, Button,TouchableOpacity } from 'react-native'
+import { ScrollView, View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native'
 import g, { p } from "../styles/global"
 import { secondsToTimeString } from '../js/timerfunctions'
 
@@ -26,9 +26,15 @@ export default function TimelineToday({ stopProject, projects, startProject, nav
                 color: project.color
             })
         })
+        console.log(relevant)
 
+        const half = relevant.length / 2;
 
-        setRelevantData(relevant)
+        
+        
+        let halfed = [relevant.slice(0, Math.ceil(half)), relevant.slice(-Math.floor(half))]
+        
+        setRelevantData(halfed)
     }, [projects])
 
 
@@ -38,54 +44,68 @@ export default function TimelineToday({ stopProject, projects, startProject, nav
         <ScrollView
             horizontal={true}
         >
-            <View>
-
+            <View
+                style={s.projectCardColumnContainer}
+            >
                 {
-                    relevantData.map((project) => {
-                        if (project.running) {
-                            const color = StyleSheet.create({
-                                c: {
-                                    backgroundColor: project.color
+                    relevantData.map((half, i) => {
+                        return (
+                            <View style={s.projectCardContainer}>
+                                {
+                                half.map((project) => {
+                                    const color = StyleSheet.create({
+                                        c: {
+                                            backgroundColor: project.color
+                                        }
+                                    })
+                                    if (project.running) {
+                                        return (
+                                            <TouchableOpacity
+                                                onPress={() => navigation.navigate("ProjectView", { projectViewPid: project.pid })}
+                                                style={[s.projectCard, color.c]}
+                                            >
+                                                <Text>{project.name}</Text>
+                                                <TouchableOpacity style={s.stopButton} onPress={() => stopProject()}>
+                                                    <Text>Stop</Text>
+                                                </TouchableOpacity>
+                                            </TouchableOpacity>
+                                        )
+                                    } else
+                                        return (
+
+                                            <TouchableOpacity
+                                                style={s.projectCard}
+                                                onPress={() => navigation.navigate("ProjectView", { projectViewPid: project.pid })}
+                                            >
+                                                <Text>{project.name}</Text>
+                                                <TouchableOpacity style={[s.startButton, color.c]} onPress={() => startProject(project.pid)}>
+                                                    <Text>{secondsToTimeString(project.total_duration.toString(10))} </Text>
+                                                </TouchableOpacity>
+                                            </TouchableOpacity>
+                                        )
+                                })
                                 }
-                            })
+                            </View>
+                        )
 
-                            return (
-                                <TouchableOpacity
-                                    onPress={() => navigation.navigate("ProjectView", { projectViewPid: project.pid })}
-                                    style={[s.projectCard, color.c]}
-                                >
-                                    <Text>{project.name}</Text>
-                                    <TouchableOpacity style={s.stopButton} onPress={() => stopProject()}>
-                                        <Text>Stop</Text>
-                                    </TouchableOpacity>
-                                </TouchableOpacity>
-                            )
-                        } else
-                            return (
-
-                                <TouchableOpacity
-                                    style={s.projectCard}
-                                    onPress={() => navigation.navigate("ProjectView", { projectViewPid: project.pid })}
-                                >
-                                    <Text>{project.name}</Text>
-                                    <TouchableOpacity style={s.startButton} onPress={() => startProject(project.pid)}>
-                                        <Text>{secondsToTimeString(project.total_duration.toString(10))} </Text>
-                                    </TouchableOpacity>
-                                </TouchableOpacity>
-                            )
                     })
                 }
-
-
-
-
-
             </View>
         </ScrollView>
     )
 }
 
 const s = StyleSheet.create({
+    projectCardContainer: {
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap"
+    },
+    projectCardColumnContainer: {
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "wrap"
+    },
     projectCard: {
         flex: 1,
         backgroundColor: p.bg2,
@@ -94,13 +114,14 @@ const s = StyleSheet.create({
         borderRadius: p.br,
         alignItems: 'center',
         justifyContent: 'center',
+        margin: 10
     },
-    startButton:{
+    startButton: {
         backgroundColor: "#ffffff20",
         padding: 10,
         borderRadius: p.br,
     },
-    stopButton:{
+    stopButton: {
         backgroundColor: "#ffffff20",
         padding: 10,
         borderRadius: p.br
