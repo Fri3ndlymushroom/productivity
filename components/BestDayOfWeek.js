e53f3a94dce5b1692659b414009f4937dfe880a7
Moritz Huesser <morihues0@gmail.com>
2021-11-16 07:39:45 +0100

    best day of week style

--- components/BestDayOfWeek.js
+++ components/BestDayOfWeek.js
@@ -7,27 +7,47 @@ export default function BestDayOfWeek({ data }) {
 
     const getBestDayOfWeek = () =>{
         let days = [
-            {day: "Mon", duration: 0},
-            {day: "Tue", duration: 0},
-            {day: "Wed", duration: 0},
-            {day: "Thu", duration: 0},
-            {day: "Fri", duration: 0},
-            {day: "Sat", duration: 0},
-            {day: "Sun", duration: 0},
+            {day: "Mon", duration: 0, distribution: 0},
+            {day: "Tue", duration: 0, distribution: 0},
+            {day: "Wed", duration: 0, distribution: 0},
+            {day: "Thu", duration: 0, distribution: 0},
+            {day: "Fri", duration: 0, distribution: 0},
+            {day: "Sat", duration: 0, distribution: 0},
+            {day: "Sun", duration: 0, distribution: 0},
         ]
         data.all_logs.forEach((log)=>{
             let index = days.findIndex((day)=>day.day === formatSeconds(log.start, "EEE"))
             days[index].duration += log.duration
         })
 
+        let tot = days.reduce((sum, day)=>sum += day.duration, 0)
+
+        days.forEach((day)=>{
+            day.distribution = Math.round(100 * day.duration / tot) === 100 ? 99 : Math.round(100 * day.duration / tot)
+        })
+
+        let stylesObj = {}
+
+        days.forEach((day)=>{
+            stylesObj[day.day] = {backgroundColor: day.distribution.toString(10).length === 1 ? "#48C645"+day.distribution.toString(10) + "0": "#48C645"+day.distribution} 
+        })
+
+        let styles = StyleSheet.create(stylesObj)
+
+        days.forEach((day)=>{
+            day.style = styles[day.day]
+        })
+
+        console.log(days)
+
         return days
     }
 
     return (
-        <View>
+        <View style={s.container}>
             {
                 getBestDayOfWeek().map((day)=>{
-                    return <Text key={day.day}>{day.day} {day.duration}</Text>
+                    return <View key={day.day} style={[s.dayCard, day.style]}><Text style={g.text}>{day.day[0]}</Text></View>
                 })
             }
         </View>
@@ -35,5 +55,20 @@ export default function BestDayOfWeek({ data }) {
 }
 
 const s = StyleSheet.create({
-
+    container:{
+        display: "flex",
+        flexDirection: "row",
+        height: "100%",
+        width: "100%",
+        justifyContent: "space-around",
+        alignItems: "center"
+    },  
+    dayCard: {
+        height: 30,
+        width: 30,
+        borderRadius: p.br,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+    }
 })
