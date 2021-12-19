bf684805daf10258bc7f857f780efad075d62179
Fri3ndlymushroom <61848398+Fri3ndlymushroom@users.noreply.github.com>
2021-12-19 23:49:01 +0100

    fixed icons

--- screens/AddProject.js
+++ screens/AddProject.js
@@ -12,157 +12,157 @@ import ProjectIcons from '../components/ProjectIcons';
 
 export default function Home({ navigation, screenProps }) {
 
-    const [newProjectName, setNewProjectName] = useState("new Project");
-    const [selectedColor, setSelectedColor] = useState("#C0392B");
-    const [selectedIcon, setSelectedIcon] = useState("cube")
+   const [newProjectName, setNewProjectName] = useState("new Project");
+   const [selectedColor, setSelectedColor] = useState("#C0392B");
+   const [selectedIcon, setSelectedIcon] = useState("cube")
 
-    let colorStyle = StyleSheet.create({
-        style: {
-            backgroundColor: selectedColor
-        }
-    })
-
-
-    const addProject = () => {
-
-        let copy = screenProps.data
+   let colorStyle = StyleSheet.create({
+      style: {
+         backgroundColor: selectedColor
+      }
+   })
 
 
-        copy.projects.push({
-            pid: "P_" + uuidv4(),
-            name: newProjectName,
-            trackings: [],
-            color: selectedColor,
-            icon: selectedIcon
-        })
-        navigation.navigate("Timeline", { closeProjectSelection: true })
+   const addProject = () => {
 
-        screenProps.setData(copy)
-    }
+      let copy = screenProps.data
 
 
-    let icons = ["cube", "add"]
-    let colors = ['#C0392B', '#E74C3C', '#9B59B6', '#8E44AD', '#2980B9']
+      copy.projects.push({
+         pid: "P_" + uuidv4(),
+         name: newProjectName,
+         trackings: [],
+         color: selectedColor,
+         icon: selectedIcon
+      })
+      navigation.navigate("Timeline", { closeProjectSelection: true })
 
-    return (
-        <View style={g.body}>
-            <NavbarStack navigation={navigation} loc={"Create Project"}></NavbarStack>
+      screenProps.setData(copy)
+   }
 
-            <Spacer height={200}/>
-            <View style={s.projectPreview}>
-                <View style={[g.logoWrapper, colorStyle.style]}>
-                    <ProjectIcons figure={selectedIcon} />
-                </View>
-                <TextInput
-                    style={[g.input, s.projectTitle]}
-                    onChangeText={setNewProjectName}
-                    value={newProjectName} />
+
+   let icons = ["cube", "triangle", "book", "sixedge", "tube", "square"]
+   let colors = ['#C0392B', '#E74C3C', '#9B59B6', '#8E44AD', '#2980B9']
+
+   return (
+      <View style={g.body}>
+         <NavbarStack navigation={navigation} loc={"Create Project"}></NavbarStack>
+
+         <Spacer height={200} />
+         <View style={s.projectPreview}>
+            <View style={[g.logoWrapper, colorStyle.style]}>
+               <ProjectIcons figure={selectedIcon} />
             </View>
-            <View style={s.selectionParent}>
-                <ScrollView style={s.selectionScroll}
-                    horizontal={true}
-                >
-                    <View style={s.selectionInner}>
-                        {
-                            colors.map(color => {
-                                let dynamic = {
-                                    backgroundColor: color,
-                                    borderWidth: (color === selectedColor ? 2 : 0),
-                                    borderColor: "white"
-                                }
-
-
-                                return (
-                                    <TouchableOpacity key={color} onPress={() => setSelectedColor(color)} style={[s.colorTile, dynamic]}>
-
-                                    </TouchableOpacity>
-                                )
-                            })
+            <TextInput
+               style={[g.input, s.projectTitle]}
+               onChangeText={setNewProjectName}
+               value={newProjectName} />
+         </View>
+         <View style={s.selectionParent}>
+            <ScrollView style={s.selectionScroll}
+               horizontal={true}
+            >
+               <View style={s.selectionInner}>
+                  {
+                     colors.map(color => {
+                        let dynamic = {
+                           backgroundColor: color,
+                           borderWidth: (color === selectedColor ? 2 : 0),
+                           borderColor: "white"
                         }
-                    </View>
-                </ScrollView>
-            </View>
 
-            <View style={s.selectionParent}>
-                <ScrollView style={s.selectionScroll}
-                    horizontal={true}
-                >
-                    <View style={s.selectionInner}>
-                        {
-                            icons.map((icon) => {
-                                let dynamic = {
-                                    borderWidth: (icon === selectedIcon ? 2 : 0),
-                                    borderColor: "white"
-                                }
-                                return (
-                                    <TouchableOpacity key={icon} onPress={() => setSelectedIcon(icon)} style={[s.logoWrapper, dynamic]}>
-                                        <ProjectIcons figure={icon} />
-                                    </TouchableOpacity>
-                                )
-                            })
+
+                        return (
+                           <TouchableOpacity key={color} onPress={() => setSelectedColor(color)} style={[s.colorTile, dynamic]}>
+
+                           </TouchableOpacity>
+                        )
+                     })
+                  }
+               </View>
+            </ScrollView>
+         </View>
+
+         <View style={s.selectionParent}>
+            <ScrollView style={s.selectionScroll}
+               horizontal={true}
+            >
+               <View style={s.selectionInner}>
+                  {
+                     icons.map((icon) => {
+                        let dynamic = {
+                           borderWidth: (icon === selectedIcon ? 2 : 0),
+                           borderColor: "white"
                         }
-                    </View>
-                </ScrollView>
-            </View>
-            <View style={{flex: 1}}></View>
-            <TouchableOpacity style={s.button} onPress={() => addProject()} ><Text style={g.text}>Add Project</Text></TouchableOpacity>
-            <Spacer height={50}/>
-        </View>
-    );
+                        return (
+                           <TouchableOpacity key={icon} onPress={() => setSelectedIcon(icon)} style={[s.logoWrapper, dynamic]}>
+                              <ProjectIcons figure={icon} />
+                           </TouchableOpacity>
+                        )
+                     })
+                  }
+               </View>
+            </ScrollView>
+         </View>
+         <View style={{ flex: 1 }}></View>
+         <TouchableOpacity style={s.button} onPress={() => addProject()} ><Text style={g.text}>Add Project</Text></TouchableOpacity>
+         <Spacer height={50} />
+      </View>
+   );
 }
 
 const s = StyleSheet.create({
 
-    iconWrapper: {
-        display: "flex",
-        flexDirection: "row"
-    },
-    projectTitle: {
-        fontSize: 16,
-    },
-    button: {
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        backgroundColor: p.bg2,
-        borderRadius: p.br,
-    },
-    projectPreview: {
-        width: 300,
-        height: 60,
-        backgroundColor: p.bg2,
-        borderRadius: p.br,
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "flex-start",
-        alignItems: "center",
-        padding: 8
-    },
-    colorTile: {
-        width: 50,
-        height: 50,
-        borderRadius: p.br,
-        marginHorizontal: 5
-    },
-    selectionInner: {
-        display: "flex",
-        flexDirection: "row",
-        height: 50,
-    },
-    selectionScroll: {
-        height: 50
-    },
-    selectionParent: {
-        height: 50,
-        marginVertical: 20
-    },
-    logoWrapper:{
-        width: 50,
-        height: 50,
-        backgroundColor: p.bg2,
-        borderRadius: p.br,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        marginHorizontal: 5
-    }
+   iconWrapper: {
+      display: "flex",
+      flexDirection: "row"
+   },
+   projectTitle: {
+      fontSize: 16,
+   },
+   button: {
+      paddingHorizontal: 15,
+      paddingVertical: 10,
+      backgroundColor: p.bg2,
+      borderRadius: p.br,
+   },
+   projectPreview: {
+      width: 300,
+      height: 60,
+      backgroundColor: p.bg2,
+      borderRadius: p.br,
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "flex-start",
+      alignItems: "center",
+      padding: 8
+   },
+   colorTile: {
+      width: 50,
+      height: 50,
+      borderRadius: p.br,
+      marginHorizontal: 5
+   },
+   selectionInner: {
+      display: "flex",
+      flexDirection: "row",
+      height: 50,
+   },
+   selectionScroll: {
+      height: 50
+   },
+   selectionParent: {
+      height: 50,
+      marginVertical: 20
+   },
+   logoWrapper: {
+      width: 50,
+      height: 50,
+      backgroundColor: p.bg2,
+      borderRadius: p.br,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      marginHorizontal: 5
+   }
 })
--- screens/EditProject.js
+++ screens/EditProject.js
@@ -11,231 +11,231 @@ import ProjectIcons from '../components/ProjectIcons';
 
 
 export default function EditProject({ navigation, screenProps }) {
-    let pid = navigation.getParam("edited_project")
+   let pid = navigation.getParam("edited_project")
 
-    let index = screenProps.data.projects.findIndex((project) => project.pid === pid)
+   let index = screenProps.data.projects.findIndex((project) => project.pid === pid)
 
-    let [project, setProject] = useState(screenProps.data.projects[index])
+   let [project, setProject] = useState(screenProps.data.projects[index])
 
-    const [popup, setPopup] = useState(false)
-    const [changed, setChanged] = useState(false)
-
-
-    const changeProjectName = (newName) => {
-        let copy = copyObject(project)
-        copy.name = newName
-        setProject(copy)
-        setChanged(true)
-    }
-
-    const changeProjectColor = (newColor) => {
-        let copy = copyObject(project)
-        copy.color = newColor
-        setProject(copy)
-        setChanged(true)
-    }
-
-    const changeProjectIcon = (newIcon) => {
-        let copy = copyObject(project)
-        copy.icon = newIcon
-        setProject(copy)
-        setChanged(true)
-    }
-
-    const saveChanges = () => {
-        let copy = copyObject(screenProps.data)
-        copy.projects[index] = project
-        screenProps.setData(copy)
-        navigation.pop()
-        setChanged(false)
-    }
+   const [popup, setPopup] = useState(false)
+   const [changed, setChanged] = useState(false)
 
 
-    const archiveProject = () => {
-        let copy = copyObject(screenProps.data)
-        let index = copy.projects.findIndex((project) => project.pid === pid)
+   const changeProjectName = (newName) => {
+      let copy = copyObject(project)
+      copy.name = newName
+      setProject(copy)
+      setChanged(true)
+   }
 
-        copy.projects[index].archived = true
+   const changeProjectColor = (newColor) => {
+      let copy = copyObject(project)
+      copy.color = newColor
+      setProject(copy)
+      setChanged(true)
+   }
 
-        copy.all_logs.forEach((log, i) => {
-            if (log.pid === pid) {
-                copy.all_logs[i].archived = true
-            }
-        })
+   const changeProjectIcon = (newIcon) => {
+      let copy = copyObject(project)
+      copy.icon = newIcon
+      setProject(copy)
+      setChanged(true)
+   }
 
-        screenProps.setData(copy)
-        navigation.pop()
-        navigation.pop()
-    }
-
-
-    const deleteProject = () => {
-        let copy = copyObject(screenProps.data)
-        let index = copy.projects.findIndex((project) => project.pid === pid)
-
-        copy.projects.splice(index, 1)
-
-        copy.all_logs = copy.all_logs.filter((log) => log.pid !== pid)
-
-        screenProps.setData(copy)
-
-        navigation.pop()
-        navigation.pop()
-    }
+   const saveChanges = () => {
+      let copy = copyObject(screenProps.data)
+      copy.projects[index] = project
+      screenProps.setData(copy)
+      navigation.pop()
+      setChanged(false)
+   }
 
 
-    let icons = ["cube", "add"]
-    let colors = ['#C0392B', '#E74C3C', '#9B59B6', '#8E44AD', '#2980B9']
+   const archiveProject = () => {
+      let copy = copyObject(screenProps.data)
+      let index = copy.projects.findIndex((project) => project.pid === pid)
 
-    return (
-        <>
-            <View style={g.body}>
-                <NavbarStack navigation={navigation} loc={"Edit Project"} saveable={true} changed={changed} saveChanges={saveChanges}></NavbarStack>
-                <Spacer height={200} />
-                <View style={s.projectPreview}>
-                    <View style={[g.logoWrapper, { backgroundColor: project.color }]}>
-                        <ProjectIcons figure={project.icon} />
-                    </View>
-                    <TextInput
-                        style={[g.input, s.projectTitle]}
-                        onChangeText={changeProjectName}
-                        value={project.name} />
-                </View>
-                <View style={s.selectionParent}>
-                    <ScrollView style={s.selectionScroll}
-                        horizontal={true}
-                    >
-                        <View style={s.selectionInner}>
-                            {
-                                colors.map(color => {
-                                    let dynamic = {
-                                        backgroundColor: color,
-                                        borderWidth: (color === project.color ? 2 : 0),
-                                        borderColor: "white"
-                                    }
+      copy.projects[index].archived = true
+
+      copy.all_logs.forEach((log, i) => {
+         if (log.pid === pid) {
+            copy.all_logs[i].archived = true
+         }
+      })
+
+      screenProps.setData(copy)
+      navigation.pop()
+      navigation.pop()
+   }
 
 
-                                    return (
-                                        <TouchableOpacity key={color} onPress={() => changeProjectColor(color)} style={[s.colorTile, dynamic]}>
+   const deleteProject = () => {
+      let copy = copyObject(screenProps.data)
+      let index = copy.projects.findIndex((project) => project.pid === pid)
 
-                                        </TouchableOpacity>
-                                    )
-                                })
-                            }
-                        </View>
-                    </ScrollView>
-                </View>
-                <View style={s.selectionParent}>
-                    <ScrollView style={s.selectionScroll}
-                        horizontal={true}
-                    >
-                        <View style={s.selectionInner}>
-                            {
-                                icons.map((icon) => {
-                                    let dynamic = {
-                                        borderWidth: (icon === project.icon ? 2 : 0),
-                                        borderColor: "white"
-                                    }
-                                    return (
-                                        <TouchableOpacity key={icon} onPress={() => changeProjectIcon(icon)} style={[s.logoWrapper, dynamic]}>
-                                            <ProjectIcons figure={icon} />
-                                        </TouchableOpacity>
-                                    )
-                                })
-                            }
-                        </View>
-                    </ScrollView>
-                </View>
-                <View style={{flex: 1}}></View>
-                <View style={s.buttonSection}>
-                    <TouchableOpacity style={s.button} onPress={()=>saveChanges()}><Text style={[g.text, g.buttonText]}>Save</Text></TouchableOpacity>
-                    <TouchableOpacity style={s.button} onPress={()=>archiveProject()}><Text style={[g.text, g.buttonText]}>Archive</Text></TouchableOpacity>
-                    <TouchableOpacity style={s.button} onPress={()=>setPopup(true)}><Text style={[g.text, g.buttonText]}>Delete</Text></TouchableOpacity>
-                </View>
-                <Spacer height={50}/>
+      copy.projects.splice(index, 1)
+
+      copy.all_logs = copy.all_logs.filter((log) => log.pid !== pid)
+
+      screenProps.setData(copy)
+
+      navigation.pop()
+      navigation.pop()
+   }
+
+
+   let icons = ["cube", "triangle", "book", "tube", "square", "sixedge"]
+   let colors = ['#C0392B', '#E74C3C', '#9B59B6', '#8E44AD', '#2980B9']
+
+   return (
+      <>
+         <View style={g.body}>
+            <NavbarStack navigation={navigation} loc={"Edit Project"} saveable={true} changed={changed} saveChanges={saveChanges}></NavbarStack>
+            <Spacer height={200} />
+            <View style={s.projectPreview}>
+               <View style={[g.logoWrapper, { backgroundColor: project.color }]}>
+                  <ProjectIcons figure={project.icon} />
+               </View>
+               <TextInput
+                  style={[g.input, s.projectTitle]}
+                  onChangeText={changeProjectName}
+                  value={project.name} />
             </View>
-            {
-                popup &&
-                <TouchableOpacity onPress={() => setPopup(false)} style={s.popup}>
-                    <Text style={g.text}>Do you really want to delete this project?</Text>
-                    <TouchableOpacity onPress={() => deleteProject()} style={g.button}>
-                        <Text style={g.text}>Delete this project permanently</Text>
-                    </TouchableOpacity>
-                </TouchableOpacity>
-            }
-        </>
-    );
+            <View style={s.selectionParent}>
+               <ScrollView style={s.selectionScroll}
+                  horizontal={true}
+               >
+                  <View style={s.selectionInner}>
+                     {
+                        colors.map(color => {
+                           let dynamic = {
+                              backgroundColor: color,
+                              borderWidth: (color === project.color ? 2 : 0),
+                              borderColor: "white"
+                           }
+
+
+                           return (
+                              <TouchableOpacity key={color} onPress={() => changeProjectColor(color)} style={[s.colorTile, dynamic]}>
+
+                              </TouchableOpacity>
+                           )
+                        })
+                     }
+                  </View>
+               </ScrollView>
+            </View>
+            <View style={s.selectionParent}>
+               <ScrollView style={s.selectionScroll}
+                  horizontal={true}
+               >
+                  <View style={s.selectionInner}>
+                     {
+                        icons.map((icon) => {
+                           let dynamic = {
+                              borderWidth: (icon === project.icon ? 2 : 0),
+                              borderColor: "white"
+                           }
+                           return (
+                              <TouchableOpacity key={icon} onPress={() => changeProjectIcon(icon)} style={[s.logoWrapper, dynamic]}>
+                                 <ProjectIcons figure={icon} />
+                              </TouchableOpacity>
+                           )
+                        })
+                     }
+                  </View>
+               </ScrollView>
+            </View>
+            <View style={{ flex: 1 }}></View>
+            <View style={s.buttonSection}>
+               <TouchableOpacity style={s.button} onPress={() => saveChanges()}><Text style={[g.text, g.buttonText]}>Save</Text></TouchableOpacity>
+               <TouchableOpacity style={s.button} onPress={() => archiveProject()}><Text style={[g.text, g.buttonText]}>Archive</Text></TouchableOpacity>
+               <TouchableOpacity style={s.button} onPress={() => setPopup(true)}><Text style={[g.text, g.buttonText]}>Delete</Text></TouchableOpacity>
+            </View>
+            <Spacer height={50} />
+         </View>
+         {
+            popup &&
+            <TouchableOpacity onPress={() => setPopup(false)} style={s.popup}>
+               <Text style={g.text}>Do you really want to delete this project?</Text>
+               <TouchableOpacity onPress={() => deleteProject()} style={g.button}>
+                  <Text style={g.text}>Delete this project permanently</Text>
+               </TouchableOpacity>
+            </TouchableOpacity>
+         }
+      </>
+   );
 }
 
 const s = StyleSheet.create({
-    popup: {
-        zIndex: 10,
-        elevation: (Platform.OS === 'android') ? 10 : 0,
-        position: "absolute",
-        top: 0,
-        bottom: 0,
-        right: 0,
-        left: 0,
-        backgroundColor: "#00000099",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        justifyContent: 'center',
-    },
-    projectPreview: {
-        width: 300,
-        height: 60,
-        backgroundColor: p.bg2,
-        borderRadius: p.br,
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "flex-start",
-        alignItems: "center",
-        padding: 8
-    },
-    projectTitle: {
-        fontSize: 16,
-    },
-    colorTile: {
-        width: 50,
-        height: 50,
-        borderRadius: p.br,
-        marginHorizontal: 5
-    },
-    selectionInner: {
-        display: "flex",
-        flexDirection: "row",
-        height: 50,
-    },
-    selectionScroll: {
-        height: 50
-    },
-    selectionParent: {
-        height: 50,
-        marginVertical: 20
-    },
-    logoWrapper: {
-        width: 50,
-        height: 50,
-        backgroundColor: p.bg2,
-        borderRadius: p.br,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        marginHorizontal: 5
-    },
-    buttonSection:{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        width: "80%"
-    },
-    button: {
-        paddingHorizontal: 5,
-        paddingVertical: 10,
-        width: 90,
-        marginHorizontal: 5,
-        backgroundColor: p.bg2,
-        borderRadius: p.br,
-    },
+   popup: {
+      zIndex: 10,
+      elevation: (Platform.OS === 'android') ? 10 : 0,
+      position: "absolute",
+      top: 0,
+      bottom: 0,
+      right: 0,
+      left: 0,
+      backgroundColor: "#00000099",
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      justifyContent: 'center',
+   },
+   projectPreview: {
+      width: 300,
+      height: 60,
+      backgroundColor: p.bg2,
+      borderRadius: p.br,
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "flex-start",
+      alignItems: "center",
+      padding: 8
+   },
+   projectTitle: {
+      fontSize: 16,
+   },
+   colorTile: {
+      width: 50,
+      height: 50,
+      borderRadius: p.br,
+      marginHorizontal: 5
+   },
+   selectionInner: {
+      display: "flex",
+      flexDirection: "row",
+      height: 50,
+   },
+   selectionScroll: {
+      height: 50
+   },
+   selectionParent: {
+      height: 50,
+      marginVertical: 20
+   },
+   logoWrapper: {
+      width: 50,
+      height: 50,
+      backgroundColor: p.bg2,
+      borderRadius: p.br,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      marginHorizontal: 5
+   },
+   buttonSection: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      width: "80%"
+   },
+   button: {
+      paddingHorizontal: 5,
+      paddingVertical: 10,
+      width: 90,
+      marginHorizontal: 5,
+      backgroundColor: p.bg2,
+      borderRadius: p.br,
+   },
 })
